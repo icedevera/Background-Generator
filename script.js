d45e94d25e2c281905f186601ed1e06370aff29d
Ice <iestyndevera@gmail.com>
2020-07-05 11:50:40 +0800

    Adding initial files

---
+++ script.js
@@ -0,0 +1,43 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var btn = document.querySelector("button");
+
+function setGradient() {
+  body.style.background =
+    "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+
+  css.textContent = body.style.background + ";";
+}
+
+function randomBackground() {
+    body.style.background =
+    "linear-gradient(to right, " 
+    + "rgb(" 
+    + Math.floor(Math.random() * 255) 
+    + "," 
+    + Math.floor(Math.random() * 255) 
+    + "," 
+    + Math.floor(Math.random() * 255)
+    + "), "
+    + "rgb(" 
+    + Math.floor(Math.random() * 255) 
+    + "," 
+    + Math.floor(Math.random() * 255) 
+    + "," 
+    + Math.floor(Math.random() * 255)
+    + "))";
+    css.textContent = body.style.background + ";";
+}
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
+body.style.background =
+  "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+
+css.textContent = body.style.background + ";";
+
+btn.addEventListener("click", randomBackground)
